8f8e793c5160a55d898ae512aa33c9da16a4cb99
Akash Sagar <AkashSagar.Ind@classichome.com>
2025-08-20 18:02:17 +0530

    added other quotation feilds

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,4 +3,28 @@ from odoo import models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    x_test_field = fields.Char(string="Test Field")  # custom field
+    cus_po_issue_date = fields.Date(string="PO Issue Date")
+    cus_ex_fact_date = fields.Date(string="Vendor Ex-Fact Date")
+    cus_proforma_number = fields.Char(string="Proforma Number")
+    cus_buyer_order_no = fields.Char(string="Buyer Order No")
+    country_origin = fields.Many2one(
+        'res.country',
+        string='Country of Origin'
+    )
+    gst_treatment = fields.Char(
+    related='partner_id.property_account_position_id.name',
+    readonly=True,
+    string='GST Treatment'
+    )
+    date_order = fields.Datetime(
+        string='Order Date',     
+        readonly=False,            
+        required=True
+    )
+    planning_days = fields.Integer(string='Planning Days')
+    rev_vendor_ex_date = fields.Date(string='Rev. Vendor Ex Date')
+    vendor_id = fields.Many2one(
+        'res.partner',
+        string='Vendor',
+        domain=[('supplier_rank', '>', 0)]
+    )
